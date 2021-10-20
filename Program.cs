e398a07ad34f79f494c43977766cb681b046cdf4
Brent Croal <brent.croal@agile-banking.com>
2021-10-20 12:54:34 -0400

    GIM Repair for not open cards

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ namespace CorServCreditCardETL
 
                             string sep = "\t";
                             string[] splitContent = str.Split(sep.ToCharArray());
-                            List<string> linein = splitContent.ToList();                            
+                            List<string> linein = splitContent.ToList();
 
                             for (int i = 0; i < productlist.Count; i++)
                             {
@@ -78,88 +78,89 @@ namespace CorServCreditCardETL
 
                                 if (CreditCardProducts == ProductName)
                                 {
-                                    if (linein.ElementAt(19) != "Lost/Stolen" && linein.ElementAt(20) != "")
+                                    string TaxId_01a = linein.ElementAt(1).Trim();
+                                    TaxId_01a = TaxId_01a.PadRight(100).Substring(0, 9);
+                                    string TaxId_01b = linein.ElementAt(2).Trim();
+                                    TaxId_01b = TaxId_01b.PadRight(100).Substring(0, 9);
+                                    string AcctNum_02 = linein.ElementAt(20).Trim();
+                                    AcctNum_02 = AcctNum_02.PadRight(100).Substring(0, 30);
+                                    string MajorCode_03 = "EXT ";
+                                    string MinorCode_04 = ProductCode;
+                                    string Name_05a = linein.ElementAt(3).Trim();
+                                    string Name_05b = linein.ElementAt(4).Trim();
+                                    if (ProductType == "Business") { PType_06 = "O"; } else { PType_06 = "P"; }
+                                    if (PType_06 == "O")
                                     {
-                                        string TaxId_01a = linein.ElementAt(1).Trim();
-                                        TaxId_01a = TaxId_01a.PadRight(100).Substring(0, 9);
-                                        string TaxId_01b = linein.ElementAt(2).Trim();
-                                        TaxId_01b = TaxId_01b.PadRight(100).Substring(0, 9);
-                                        string AcctNum_02 = linein.ElementAt(20).Trim();
-                                        AcctNum_02 = AcctNum_02.PadRight(100).Substring(0, 30);
-                                        string MajorCode_03 = "EXT ";
-                                        string MinorCode_04 = ProductCode;
-                                        string Name_05a = linein.ElementAt(3).Trim();
-                                        string Name_05b = linein.ElementAt(4).Trim();
-                                        if (ProductType == "Business") { PType_06 = "O"; } else { PType_06 = "P"; }
-                                        if (PType_06 == "O")
-                                        {
-                                            Name01out = Name_05a.PadRight(100).Substring(0, 40);
+                                        Name01out = Name_05a.PadRight(100).Substring(0, 40);
 
-                                            if (Name_05b.Trim() != "")
-                                            {
-                                                Name02out = Name_05b.PadRight(100).Substring(0, 40);
-                                            }
-                                        }
-                                        else
+                                        if (Name_05b.Trim() != "")
                                         {
-                                            sep = " ";
-                                            Name01split = Name_05a.Split(sep.ToCharArray());
-                                            int result1 = Name_05a.ToCharArray().Count(c => c == ' ');
-                                            Name01out = Name01split.ElementAt(0).PadRight(100).Substring(0, 20) + Name01split.ElementAt(result1).PadRight(100).Substring(0, 20);
-
-                                            if (Name_05b.Trim() != "")
-                                            {
-                                                Name02split = Name_05b.Split(sep.ToCharArray());
-                                                int result2 = Name_05b.ToCharArray().Count(c => c == ' ');
-                                                Name02out = Name02split.ElementAt(0).PadRight(100).Substring(0, 20) + Name02split.ElementAt(result2).PadRight(100).Substring(0, 20);
-                                            }
+                                            Name02out = Name_05b.PadRight(100).Substring(0, 40);
                                         }
-                                        string Gap_07 = "".PadRight(4);
-                                        string AddrLine1_08 = linein.ElementAt(5).Trim();
-                                        AddrLine1_08 = AddrLine1_08.PadRight(100).Substring(0, 40);
-                                        string AddrLine2_09 = linein.ElementAt(6).Trim();
-                                        AddrLine2_09 = AddrLine2_09.PadRight(100).Substring(0, 40);
-                                        string Gap_10 = "".PadRight(40);
-                                        string City_11 = linein.ElementAt(7).Trim();
-                                        City_11 = City_11.PadRight(100).Substring(0, 30);
-                                        string State_12 = linein.ElementAt(8).Trim();
-                                        State_12 = State_12.PadRight(100).Substring(0, 2);
-                                        string Zipcode_13 = linein.ElementAt(9).Trim();
-                                        Zipcode_13 = Zipcode_13.PadRight(100).Substring(0, 5);
-                                        string Gap_14 = "".PadRight(21);
-                                        string Arecode_15 = linein.ElementAt(10).Trim();
-                                        Arecode_15 = Arecode_15.PadRight(100).Substring(0, 3);
-                                        string Exchange_16 = linein.ElementAt(10).Trim();
-                                        Exchange_16 = Exchange_16.PadRight(100).Substring(3, 3);
-                                        string Phone_17 = linein.ElementAt(10).Trim();
-                                        Phone_17 = Phone_17.PadRight(100).Substring(6, 4);
-                                        string Gap_18 = "".PadRight(10);
-                                        string Intrate_19 = Convert.ToString(Convert.ToDecimal(linein.ElementAt(13)) ).PadRight(100).Substring(0, 15);
-                                        string Gap_20 = "".PadRight(15);
-                                        string AvailableCredit_21 = linein.ElementAt(17).Trim();
-                                        AvailableCredit_21 = AvailableCredit_21.PadRight(100).Substring(0, 15);
-                                        string DateOpen_22 = linein.ElementAt(18).Trim();
-                                        DateOpen_22 = DateOpen_22.Replace("/", "").PadRight(100).Substring(0, 8);
-                                        string MatDate_23 = linein.ElementAt(25).Trim();
-                                        MatDate_23 = MatDate_23.Replace("/", "").PadRight(100).Substring(0, 8);
-                                        string Gap_24 = "".PadRight(16);
-                                        string Gap_25 = "".PadRight(8);
-                                        string Gap_26 = "".PadRight(8);
-                                        string CreditLimit_27 = linein.ElementAt(15).Trim(); //was 16
-                                        CreditLimit_27 = CreditLimit_27.PadRight(100).Substring(0, 15);
-                                        string Gap_28 = "".PadRight(8);
-                                        string Gap_29 = "".PadRight(15);
-                                        string PaymentDue_30 = linein.ElementAt(29).Trim();
-                                        PaymentDue_30 = PaymentDue_30.Replace("/", "").PadRight(100).Substring(0, 8);
-                                        string MinPayment_31 = linein.ElementAt(27).Trim();
-                                        MinPayment_31 = MinPayment_31.PadRight(100).Substring(0, 15);
-                                        string Gap_32 = "".PadRight(94);
-                                        string Static_33 = "NOTE/BAL ";
-                                        string CurrentBal_34 = linein.ElementAt(14).Trim(); //was 15
-                                        CurrentBal_34 = CurrentBal_34.PadRight(100).Substring(0, 15);
-                                        string Datadate_35 = linein.ElementAt(0).Trim();
-                                        Datadate_35 = Datadate_35.Replace("/", "").PadRight(100).Substring(0, 8);
-                                        string Gap_36 = "".PadRight(1389);
+                                    }
+                                    else
+                                    {
+                                        sep = " ";
+                                        Name01split = Name_05a.Split(sep.ToCharArray());
+                                        int result1 = Name_05a.ToCharArray().Count(c => c == ' ');
+                                        Name01out = Name01split.ElementAt(0).PadRight(100).Substring(0, 20) + Name01split.ElementAt(result1).PadRight(100).Substring(0, 20);
+
+                                        if (Name_05b.Trim() != "")
+                                        {
+                                            Name02split = Name_05b.Split(sep.ToCharArray());
+                                            int result2 = Name_05b.ToCharArray().Count(c => c == ' ');
+                                            Name02out = Name02split.ElementAt(0).PadRight(100).Substring(0, 20) + Name02split.ElementAt(result2).PadRight(100).Substring(0, 20);
+                                        }
+                                    }
+                                    string Gap_07 = "".PadRight(4);
+                                    string AddrLine1_08 = linein.ElementAt(5).Trim();
+                                    AddrLine1_08 = AddrLine1_08.PadRight(100).Substring(0, 40);
+                                    string AddrLine2_09 = linein.ElementAt(6).Trim();
+                                    AddrLine2_09 = AddrLine2_09.PadRight(100).Substring(0, 40);
+                                    string Gap_10 = "".PadRight(40);
+                                    string City_11 = linein.ElementAt(7).Trim();
+                                    City_11 = City_11.PadRight(100).Substring(0, 30);
+                                    string State_12 = linein.ElementAt(8).Trim();
+                                    State_12 = State_12.PadRight(100).Substring(0, 2);
+                                    string Zipcode_13 = linein.ElementAt(9).Trim();
+                                    Zipcode_13 = Zipcode_13.PadRight(100).Substring(0, 5);
+                                    string Gap_14 = "".PadRight(21);
+                                    string Arecode_15 = linein.ElementAt(10).Trim();
+                                    Arecode_15 = Arecode_15.PadRight(100).Substring(0, 3);
+                                    string Exchange_16 = linein.ElementAt(10).Trim();
+                                    Exchange_16 = Exchange_16.PadRight(100).Substring(3, 3);
+                                    string Phone_17 = linein.ElementAt(10).Trim();
+                                    Phone_17 = Phone_17.PadRight(100).Substring(6, 4);
+                                    string Gap_18 = "".PadRight(10);
+                                    string Intrate_19 = Convert.ToString(Convert.ToDecimal(linein.ElementAt(13))).PadRight(100).Substring(0, 15);
+                                    string Gap_20 = "".PadRight(15);
+                                    string AvailableCredit_21 = linein.ElementAt(17).Trim();
+                                    AvailableCredit_21 = AvailableCredit_21.PadRight(100).Substring(0, 15);
+                                    string DateOpen_22 = linein.ElementAt(18).Trim();
+                                    DateOpen_22 = DateOpen_22.Replace("/", "").PadRight(100).Substring(0, 8);
+                                    string MatDate_23 = linein.ElementAt(25).Trim();
+                                    MatDate_23 = MatDate_23.Replace("/", "").PadRight(100).Substring(0, 8);
+                                    string Gap_24 = "".PadRight(16);
+                                    string Gap_25 = "".PadRight(8);
+                                    string Gap_26 = "".PadRight(8);
+                                    string CreditLimit_27 = linein.ElementAt(15).Trim(); //was 16
+                                    CreditLimit_27 = CreditLimit_27.PadRight(100).Substring(0, 15);
+                                    string Gap_28 = "".PadRight(8);
+                                    string Gap_29 = "".PadRight(15);
+                                    string PaymentDue_30 = linein.ElementAt(29).Trim();
+                                    PaymentDue_30 = PaymentDue_30.Replace("/", "").PadRight(100).Substring(0, 8);
+                                    string MinPayment_31 = linein.ElementAt(27).Trim();
+                                    MinPayment_31 = MinPayment_31.PadRight(100).Substring(0, 15);
+                                    string Gap_32 = "".PadRight(94);
+                                    string Static_33 = "NOTE/BAL ";
+                                    string CurrentBal_34 = linein.ElementAt(14).Trim(); //was 15
+                                    CurrentBal_34 = CurrentBal_34.PadRight(100).Substring(0, 15);
+                                    string Datadate_35 = linein.ElementAt(0).Trim();
+                                    Datadate_35 = Datadate_35.Replace("/", "").PadRight(100).Substring(0, 8);
+                                    string Gap_36 = "".PadRight(1389);
+
+                                    if (linein.ElementAt(19) == "Open" && linein.ElementAt(20) != "")
+                                    {
                                         string lineout = TaxId_01a + AcctNum_02 + MajorCode_03 + MinorCode_04 + Name01out + PType_06 + Gap_07 + AddrLine1_08 + AddrLine2_09 + Gap_10 + City_11 + State_12 + Zipcode_13 + Gap_14 + Arecode_15 + Exchange_16 + Phone_17 + Gap_18 + Intrate_19 + Gap_20 + AvailableCredit_21 + DateOpen_22 + MatDate_23 + Gap_24 + Gap_25 + Gap_26 + CreditLimit_27 + Gap_28 + Gap_29 + PaymentDue_30 + MinPayment_31 + Gap_32 + Static_33 + CurrentBal_34 + Datadate_35 + Gap_36;
                                         /*
                                         int x = 0;
@@ -177,7 +178,22 @@ namespace CorServCreditCardETL
                                             File.AppendAllText(useOutfile, lineout2.Replace("\n", null).Replace("\r", null) + "\r\n");
                                         }
                                     }
-                                    
+                                    else
+                                    {
+                                        if (linein.ElementAt(19) != "Open")
+                                        {
+                                            if (Name01out == "".PadRight(100).Substring(0, 40) || Name01out is null)
+                                            {
+                                                string CardLine = "WF GIM Repair".PadRight(100).Substring(0, 20) + "NOTIFY_CARD_NOT_OPEN".PadRight(100).Substring(0, 25) + "||" + AcctNum_02 + "|" + TaxId_01a + "-" + Name02out + "\n";
+                                                File.AppendAllText(useErrorfile, CardLine);
+                                            }
+                                            else
+                                            {
+                                                string CardLine = "WF GIM Repair".PadRight(100).Substring(0, 20) + "NOTIFY_CARD_NOT_OPEN".PadRight(100).Substring(0, 25) + "||" + AcctNum_02 + "|" + TaxId_01a + "-" + Name01out + "\n";
+                                                File.AppendAllText(useErrorfile, CardLine);
+                                            }
+                                        }
+                                    }
                                     found = 1;
                                 }
                             }
@@ -209,7 +225,7 @@ namespace CorServCreditCardETL
                 catch (Exception ex)
                 {
                     string error = ex.ToString();
-                    string ErrorLine = "WF GIM Repair".PadRight(100).Substring(0, 20) + "Notification".PadRight(100).Substring(0, 25) + "||CORSERV|" + error.Substring(0, 230);
+                    string ErrorLine = "WF GIM Repair".PadRight(100).Substring(0, 20) + "Notification".PadRight(100).Substring(0, 25) + "||CORSERV|" + error.Replace("\n", " ").Replace("\r", " ").Substring(0, 230);
                     File.AppendAllText(useErrorfile, ErrorLine);
 
                     Console.WriteLine("Error: " + ex);
